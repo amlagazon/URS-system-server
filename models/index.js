7276b3e216197147e27995d484b911135b92ef26
Maureen Batacan <mnbatacan@up.edu.ph>
2021-01-19 20:10:21 +0800

    Bring back constraints:false
    This change addresses the need by:
    
    *

--- models/index.js
+++ models/index.js
@@ -30,33 +30,33 @@ db.semester = require("./semesters.js")(sequelize, Sequelize);
 db.subject = require("./subjects.js")(sequelize, Sequelize);
 db.studentSubject = require("./student.subjects.js")(sequelize, Sequelize);
 
-db.userType.hasMany(db.user, { foreignKey: "user_type_id" });
-db.user.belongsTo(db.userType, { foreignKey: "user_type_id" });
-db.user.belongsTo(db.student, { foreignKey: "student_id" });
-db.user.belongsTo(db.admin, { foreignKey: "admin_id" });
-db.user.belongsTo(db.evaluator, { foreignKey: "evaluator_id" });
+db.userType.hasMany(db.user, { foreignKey: "user_type_id", constraints: false  });
+db.user.belongsTo(db.userType, { foreignKey: "user_type_id", constraints: false });
+db.user.belongsTo(db.student, { foreignKey: "student_id", constraints: false });
+db.user.belongsTo(db.admin, { foreignKey: "admin_id", constraints: false });
+db.user.belongsTo(db.evaluator, { foreignKey: "evaluator_id", constraints: false });
 
-db.evaluator.hasOne(db.user, { foreignKey: "evaluator_id" });
+db.evaluator.hasOne(db.user, { foreignKey: "evaluator_id", constraints: false  });
 // db.evaluator.belongsTo(db.program, { foreignKey: "program_id" });
-db.evaluator.belongsTo(db.programCourse, { foreignKey: "program_course_id" });
+db.evaluator.belongsTo(db.programCourse, { foreignKey: "program_course_id", constraints: false });
 
-db.student.belongsTo(db.programCourse, { foreignKey: "program_course_id" });
-db.student.hasMany(db.studentSubject, { foreignKey: "student_id" });
+db.student.belongsTo(db.programCourse, { foreignKey: "program_course_id", constraints: false });
+db.student.hasMany(db.studentSubject, { foreignKey: "student_id", constraints: false  });
 db.student.hasOne(db.user, { foreignKey: "student_id" });
 
 // db.program.hasMany(db.evaluator, { foreignKey: "program_id" });
-db.semester.hasOne(db.program, { foreignKey: "semester_id" });
-db.program.belongsTo(db.semester, { foreignKey: "semester_id" });
+db.semester.hasOne(db.program, { foreignKey: "semester_id", constraints: false  });
+db.program.belongsTo(db.semester, { foreignKey: "semester_id", constraints: false });
 
-db.studentSubject.belongsTo(db.semester, { foreignKey: "semester_id" });
-db.studentSubject.belongsTo(db.student, { foreignKey: "student_id" });
-db.studentSubject.belongsTo(db.subject, { foreignKey: "subject_id" });
+db.studentSubject.belongsTo(db.semester, { foreignKey: "semester_id", constraints: false });
+db.studentSubject.belongsTo(db.student, { foreignKey: "student_id", constraints: false });
+db.studentSubject.belongsTo(db.subject, { foreignKey: "subject_id", constraints: false });
 
-db.semester.hasMany(db.studentSubject, { foreignKey: "semester_id" });
+db.semester.hasMany(db.studentSubject, { foreignKey: "semester_id", constraints: false  });
 
-db.programCourse.belongsTo(db.program, { foreignKey: "program_id" });
-db.programCourse.hasMany(db.student, { foreignKey: "program_course_id" });
-db.programCourse.hasMany(db.evaluator, { foreignKey: "program_course_id" });
+db.programCourse.belongsTo(db.program, { foreignKey: "program_id", constraints: false });
+db.programCourse.hasMany(db.student, { foreignKey: "program_course_id", constraints: false  });
+db.programCourse.hasMany(db.evaluator, { foreignKey: "program_course_id", constraints: false  });
 
 // db.subject.belongsTo(db.program, { foreignKey: "program_id" });
 
@@ -64,12 +64,14 @@ db.programCourse.belongsToMany(db.subject, {
   through: "program_course_subjects",
   as: "subjects",
   foreignKey: "program_course_id",
+  constraints: false 
 });
 
 db.subject.belongsToMany(db.programCourse, {
   through: "program_course_subjects",
   as: "program_courses",
   foreignKey: "subject_id",
+  constraints: false 
 });
 
 module.exports = db;
